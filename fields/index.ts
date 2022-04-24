a45733ca12e043888be1e3bc3dc4eb0b93c160b4
keleris32 <charles.ejiegbu@gmail.com>
2022-04-24 22:45:05 +0100

    feat: added fields to project

---
+++ fields/index.ts
@@ -0,0 +1,5 @@
+export { default as meta } from './meta';
+export { default as slug } from './slug';
+export { default as link } from './link';
+export { default as backgroundColor } from './backgroundColor';
+export { default as stat } from './stat';
---
+++ fields/meta.ts
@@ -0,0 +1,34 @@
+import { Field } from 'payload/types';
+
+export type Type = {
+  meta?: {
+    title?: string;
+    description?: string;
+    keywords?: string;
+  };
+};
+
+const meta: Field = {
+  name: 'meta',
+  label: 'Meta',
+  type: 'group',
+  fields: [
+    {
+      name: 'title',
+      label: 'Title',
+      type: 'text',
+    },
+    {
+      name: 'description',
+      label: 'Description',
+      type: 'textarea',
+    },
+    {
+      name: 'keywords',
+      label: 'Keywords',
+      type: 'text',
+    },
+  ],
+};
+
+export default meta;
