1fdd251ff6147632c168866dcce60c1f2c94407a
keleris32 <charles.ejiegbu@gmail.com>
2022-04-24 22:48:28 +0100

    config: updated payload config

--- payload.config.ts
+++ payload.config.ts
@@ -1,14 +1,12 @@
 import { buildConfig } from 'payload/config';
 import dotenv from 'dotenv';
-import Page from './collections/Page';
-import Media from './collections/Media';
+import { Page, Study, Media, FormSubmission, Category } from './collections';
+import { MegaMenu, SocialMedia, Footer } from './globals';
 
 dotenv.config();
 
 export default buildConfig({
   serverURL: process.env.PAYLOAD_PUBLIC_SERVER_URL,
-  collections: [
-    Page,
-    Media,
-  ],
+  collections: [Page, Study, Media, FormSubmission, Category],
+  globals: [MegaMenu, SocialMedia, Footer],
 });
