5e7221a4b594165641e5a84d6d5a884d6fd47297
keleris32 <charles.ejiegbu@gmail.com>
2022-05-02 20:35:50 +0100

    feat: added app grid

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,100 @@
-import React from 'react';
-import type { AppProps } from 'next/app';
+/* eslint-disable comma-dangle */
+import React, { useEffect } from 'react';
+import App from 'next/app';
+import { WindowInfoProvider } from '@faceless-ui/window-info';
+import { ModalProvider } from '@faceless-ui/modal';
+import { GridProvider } from '@faceless-ui/css-grid';
+import useStyles from '../css/app';
+import Header from '../components/Layout/Header';
+import zIndex from '../css/zIndex';
+import breakpoints from '../css/breakpoints';
+import { base } from '../css/base';
+import { Type as MegaMenuType } from '../globals/MegaMenu';
+import { Type as FooterType } from '../globals/Footer';
+import { Type as SocialMediaType } from '../globals/SocialMedia';
 
-import '../css/style.css';
+type AppProps = {
+  pageProps: unknown;
+  Component: React.FC<{ footer: FooterType; socialMedia: SocialMediaType }>;
+} & {
+  megaMenu: MegaMenuType;
+  footer: FooterType;
+  socialMedia: SocialMediaType;
+};
 
-const MyApp = ({ Component, pageProps }: AppProps): React.ReactElement => (
-  <Component {...pageProps} />
-);
+const MyApp = (appProps: AppProps): React.ReactElement => {
+  const classes = useStyles();
+  const { Component, pageProps, megaMenu, footer, socialMedia } = appProps;
+
+  useEffect(() => {
+    const style = document.getElementById('server-side-styles');
+
+    if (style) {
+      style.parentNode.removeChild(style);
+    }
+  }, []);
+
+  return (
+    <WindowInfoProvider breakpoints={breakpoints}>
+      <ModalProvider zIndex={zIndex.modal}>
+        <GridProvider
+          rowGap={{
+            s: base(2),
+            m: base(2),
+            l: base(2),
+            xl: base(2),
+          }}
+          colGap={{
+            s: base(2),
+            m: base(2),
+            l: base(2),
+            xl: base(2),
+          }}
+          cols={{
+            s: 8,
+            m: 8,
+            l: 12,
+            xl: 12,
+          }}
+        >
+          <div className={classes.app}>
+            {
+              // eslint-disable-next-line react/jsx-max-props-per-line
+              <Header megaMenu={megaMenu} socialMedia={socialMedia} />
+            }
+            <Component
+              {...pageProps}
+              footer={footer}
+              socialMedia={socialMedia}
+            />
+          </div>
+        </GridProvider>
+      </ModalProvider>
+    </WindowInfoProvider>
+  );
+};
+
+MyApp.getInitialProps = async (appContext) => {
+  const appProps = await App.getInitialProps(appContext);
+
+  const [megaMenu, footer, socialMedia] = await Promise.all([
+    fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/app/globals/mega-menu`).then(
+      (res) => res.json()
+    ),
+    fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/app/globals/footer`).then(
+      (res) => res.json()
+    ),
+    fetch(
+      `${process.env.NEXT_PUBLIC_SERVER_URL}/app/globals/social-media`
+    ).then((res) => res.json()),
+  ]);
+
+  return {
+    ...appProps,
+    megaMenu,
+    footer,
+    socialMedia,
+  };
+};
 
 export default MyApp;
