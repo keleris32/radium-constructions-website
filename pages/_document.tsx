9928bea724dac3986c2ee863e056fea2e1d5af63
keleris32 <charles.ejiegbu@gmail.com>
2022-05-02 20:32:43 +0100

    feat: configured React-jss for next app

---
+++ pages/_document.tsx
@@ -0,0 +1,38 @@
+/* eslint-disable react/jsx-max-props-per-line */
+/* eslint-disable implicit-arrow-linebreak */
+import React from 'react';
+import Document from 'next/document';
+import { jss, SheetsRegistry, JssProvider, createGenerateId } from 'react-jss';
+import globalPlugin from 'jss-global';
+
+// eslint-disable-next-line import/no-default-export
+export default class JssDocument extends Document {
+  static async getInitialProps(ctx) {
+    const registry = new SheetsRegistry();
+    const generateId = createGenerateId();
+    jss.use(globalPlugin());
+    const originalRenderPage = ctx.renderPage;
+
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: (App) => (props) =>
+          (
+            <JssProvider registry={registry} generateId={generateId}>
+              <App {...props} />
+            </JssProvider>
+          ),
+      });
+
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return {
+      ...initialProps,
+      styles: (
+        <React.Fragment>
+          {initialProps.styles}
+          <style id="server-side-styles">{registry.toString()}</style>
+        </React.Fragment>
+      ),
+    };
+  }
+}
