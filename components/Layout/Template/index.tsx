03feeaa07366a71785e342dfe6a53d764afef88f
keleris32 <charles.ejiegbu@gmail.com>
2022-05-08 00:57:33 +0100

    feat: updated app UI templated component

--- components/Layout/Template/index.tsx
+++ components/Layout/Template/index.tsx
@@ -1,10 +1,18 @@
+/* eslint-disable react/jsx-max-props-per-line */
 import React from 'react';
 import Footer from '../Footer';
+import { Type as FooterType } from '../../../globals/Footer';
+import { Type as SocialMediaType } from '../../../globals/SocialMedia';
 
-const Template: React.FC = ({ children }) => (
+type Props = {
+  footer: FooterType;
+  socialMedia: SocialMediaType;
+};
+
+const Template: React.FC<Props> = ({ children, footer, socialMedia }) => (
   <main>
     {children}
-    <Footer />
+    <Footer footer={footer} socialMedia={socialMedia} />
   </main>
 );
 
