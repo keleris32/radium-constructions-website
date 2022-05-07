337e07e7743a962e6e015481a1fd06fe3095ee7d
keleris32 <charles.ejiegbu@gmail.com>
2022-05-08 00:56:58 +0100

    feat: updated GridContainer component

--- components/Layout/GridContainer/index.tsx
+++ components/Layout/GridContainer/index.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import useStyles from './css';
 
-const GridContainer: React.FC = ({ children }) => {
+type Props = {
+  className?: string;
+};
+
+const GridContainer: React.FC<Props> = ({ children, className }) => {
   const classes = useStyles();
 
-  return <div className={classes.gridContainer}>{children}</div>;
+  return (
+    <div
+      className={[classes.gridContainer, className].filter(Boolean).join(' ')}
+    >
+      {children}
+    </div>
+  );
 };
 
 export default GridContainer;
