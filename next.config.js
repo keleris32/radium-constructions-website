280857efbf728eaa13940a146f01f3d8c2599a23
keleris32 <charles.ejiegbu@gmail.com>
2022-05-12 21:30:30 +0100

    config: updated nextjs config

--- next.config.js
+++ next.config.js
@@ -1,15 +1,15 @@
 require('dotenv').config();
-const { sizes } = require('./blocks/Image/sizes');
+// const { sizes } = require('./blocks/Image/sizes');
 
 module.exports = {
   publicRuntimeConfig: {
     SERVER_URL: process.env.PAYLOAD_PUBLIC_SERVER_URL,
   },
-  images: {
-    domains: [
-      'localhost',
-      // Your domain(s) here
-    ],
-    deviceSizes: sizes,
-  },
+  // images: {
+  //   domains: [
+  //     'localhost',
+  //     // Your domain(s) here
+  //   ],
+  //   deviceSizes: sizes,
+  // },
 };
